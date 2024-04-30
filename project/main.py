d683539e4175388506f0a1094f88324e928b6eb4
KobilyanskyS <kobilyansky.s@gmail.com>
2024-04-30 20:19:45 +0300

    На главной появились категории со ссылками на товары. Добавлена страница с товарами категории

--- project/main.py
+++ project/main.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+from .admin import get_categories_hierarchically
+from . import db
+from .models import Product, Category
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
@@ -8,7 +11,15 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    categories = get_categories_hierarchically()
+    return render_template('index.html', categories=categories)
+
+
+@main.route('/products')
+def products():
+    category_id = request.args.get('category_id')
+    products_in_category = db.session.query(Product).join(Category).filter(Category.id == category_id).all()
+    return render_template('products.html', products=products_in_category, category_id=category_id)
 
 
 @main.route('/profile')
